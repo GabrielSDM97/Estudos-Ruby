cd261923484ca5e8d4f1ac40c640c7ae3c1b0358
Gabriel Sarmento <bielestudos5@proton.me>
2026-09-11 11:11:48 -0300

    First commit

---
+++ Aula 2 - Variáveis e Tipos/main.rb
@@ -0,0 +1,16 @@
+# Ruby é dinamicamente tipado, assim como Python
+
+nome = "Gabriel" # string
+idade= 2 # integer
+altura = 1.5 # float
+permissao = true # ou false
+valor_nulo = nil # Equivale a Null em Python
+
+# Tudo é objeto em Ruby, assim como é em Python.
+puts nome.class
+puts idade.class
+puts altura.class
+puts permissao.class
+puts valor_nulo.class
+
+
